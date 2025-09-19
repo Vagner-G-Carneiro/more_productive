0446faf830cb326028b417df07866e1948733f58
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-09-19 15:27:45 -0300

    Implementado todo sistema de salvamento de tarefas até o  banco de dados.

--- src/main/java/br/com/moreproductive/service/TarefaService.java
+++ src/main/java/br/com/moreproductive/service/TarefaService.java
@@ -1,7 +1,31 @@
 package br.com.moreproductive.service;
 
+import br.com.moreproductive.dto.TarefaDTO;
+import br.com.moreproductive.entities.Tarefa;
+import br.com.moreproductive.exceptions.PersistenciaException;
+import br.com.moreproductive.repository.TarefaRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TarefaService {
+
+    private final TarefaRepository tarefaRepository;
+
+    public TarefaService(TarefaRepository tarefaRepository)
+    {
+        this.tarefaRepository = tarefaRepository;
+    }
+
+    public TarefaDTO salvar(TarefaDTO novaTarefa)
+    {
+        try
+        {
+            Tarefa tarefa = new Tarefa(novaTarefa);
+            tarefa = this.tarefaRepository.save(tarefa);
+            return new TarefaDTO(tarefa);
+        } catch (Exception e) {
+            throw new PersistenciaException("Erro ao persistir tarefa no banco: " + e.getMessage());
+        }
+    }
+
 }
