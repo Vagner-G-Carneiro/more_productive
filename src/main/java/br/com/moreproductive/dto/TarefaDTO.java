a8d64cfa59f644173ea976d51ba6cecdb19dec79
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-09-18 23:52:07 -0300

    implementação de Entity e DTO Tarefa

--- src/main/java/br/com/moreproductive/dto/TarefaDTO.java
+++ src/main/java/br/com/moreproductive/dto/TarefaDTO.java
@@ -1,4 +1,86 @@
 package br.com.moreproductive.dto;
 
+import br.com.moreproductive.enums.PrioridadeTarefaEnum;
+import br.com.moreproductive.enums.StatusTarefaEnum;
+
+import java.time.LocalDateTime;
+
 public class TarefaDTO {
+
+    private String titulo;
+    private String descricao;
+    private StatusTarefaEnum status;
+    private PrioridadeTarefaEnum prioridade;
+    private LocalDateTime dataCriacao;
+    private LocalDateTime dataConclusao;
+    private LocalDateTime dataLimite;
+    private int usuarioId;
+
+
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public StatusTarefaEnum getStatus() {
+        return status;
+    }
+
+    public void setStatus(StatusTarefaEnum status) {
+        this.status = status;
+    }
+
+    public PrioridadeTarefaEnum getPrioridade() {
+        return prioridade;
+    }
+
+    public void setPrioridade(PrioridadeTarefaEnum prioridade) {
+        this.prioridade = prioridade;
+    }
+
+    public LocalDateTime getDataCriacao() {
+        return dataCriacao;
+    }
+
+    public void setDataCriacao(LocalDateTime dataCriacao) {
+        this.dataCriacao = dataCriacao;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDateTime dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public LocalDateTime getDataLimite() {
+        return dataLimite;
+    }
+
+    public void setDataLimite(LocalDateTime dataLimite) {
+        this.dataLimite = dataLimite;
+    }
+
+    public int getUsuarioId() {
+        return usuarioId;
+    }
+
+    public void setUsuarioId(int usuarioId) {
+        this.usuarioId = usuarioId;
+    }
+
+
 }
---
+++ src/main/java/br/com/moreproductive/enums/StatusTarefaEnum.java
@@ -0,0 +1,7 @@
+package br.com.moreproductive.enums;
+
+public enum StatusTarefaEnum {
+    ATRASADA,
+    PENDENTE,
+    CONCLUIDA,
+}
