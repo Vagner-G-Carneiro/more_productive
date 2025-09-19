229ccb5645e22a582b181a45cef0314b96519fc9
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-09-19 00:49:22 -0300

    Enums atualizados com numero para ordenação (Tipo peso em cada Enum :D)

--- src/main/java/br/com/moreproductive/enums/PrioridadeTarefaEnum.java
+++ src/main/java/br/com/moreproductive/enums/PrioridadeTarefaEnum.java
@@ -1,8 +1,20 @@
 package br.com.moreproductive.enums;
 
 public enum PrioridadeTarefaEnum {
-    BAIXA,
-    MEDIA,
-    ALTA,
-    MUITO_ALTA
+    BAIXA(1),
+    MEDIA(2),
+    ALTA(3),
+    MUITO_ALTA(4);
+
+    private final int prioridade;
+
+    PrioridadeTarefaEnum(int prioridade)
+    {
+        this.prioridade = prioridade;
+    }
+
+    public int getPrioridade()
+    {
+        return prioridade;
+    }
 }
--- src/main/java/br/com/moreproductive/enums/StatusTarefaEnum.java
+++ src/main/java/br/com/moreproductive/enums/StatusTarefaEnum.java
@@ -1,7 +1,19 @@
 package br.com.moreproductive.enums;
 
 public enum StatusTarefaEnum {
-    ATRASADA,
-    PENDENTE,
-    CONCLUIDA,
+    ATRASADA(1),
+    PENDENTE(2),
+    CONCLUIDA(3);
+
+    private final int status;
+
+    StatusTarefaEnum(int status)
+    {
+        this.status = status;
+    }
+
+    public int getOrdenacaoSimples()
+    {
+        return status;
+    }
 }
