31089e8c1ca7b23fed3609cb87f6a39e7656764a
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-10-31 12:22:26 -0300

    docs:Algumas mudanças como essa me parecem exageradas precisa commitar

--- src/main/java/br/com/moreproductive/dto/UsuarioSenhaUpdateDTO.java
+++ src/main/java/br/com/moreproductive/dto/UsuarioSenhaUpdateDTO.java
@@ -12,7 +12,7 @@ public record UsuarioSenhaUpdateDTO(
         @NotBlank(message = "A senha nova parece não estar preenchida")
         @Size(min= 8, message = "A senha deve ter no mínimo 8 caracteres, tente uma maior!")
         @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*[$*&@#]).{8,}$", message = "A senha deve conter ao menos uma letra maiuscula, " +
-        "uma minuscula, e  um caractere especial!")
+                "uma minuscula, e  um caractere especial!")
         String senhaNova,
 
         @NotBlank(message = "E-mail atual é um campo obrigatório e precisa ser preenchido para troca de credenciais.")
