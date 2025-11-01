ef5b9cedbdfd1a5777122f3f7ac2d5740e4799d2
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-11-01 18:48:54 -0300

    refactor(buscar tarefas): Mudança na lógica de como o sistema busca tarefas
    Antes: Sistema usava inúmeros end-points para requisições de diferentes buscas, o que além de ineficiente era impossível escalar caso as Tarefas tivessem mais atributos que impactassem os filtros
    
    Agora: Sistema implementa Pageable e Sort para requisições vindas com filtros escolhidos no Front-End, além de eficiente e escalável, também é seguro devido ao Spring, também otimizou tudo em um único end point e retorna o tipo Page<TarefasDTO>

--- src/main/java/br/com/moreproductive/repository/TarefaRepository.java
+++ src/main/java/br/com/moreproductive/repository/TarefaRepository.java
@@ -3,6 +3,8 @@ package br.com.moreproductive.repository;
 import br.com.moreproductive.entities.Tarefa;
 import br.com.moreproductive.enums.PrioridadeTarefaEnum;
 import br.com.moreproductive.enums.StatusTarefaEnum;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
@@ -10,19 +12,9 @@ import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
 import java.util.List;
-import java.util.Optional;
 
 @Repository
 public interface TarefaRepository extends JpaRepository <Tarefa, Integer> {
 
-    List<Tarefa> findByUsuarioIdOrderByDataLimite(int usuarioId);
-    List<Tarefa> findByUsuarioIdOrderByDataCriacaoDesc(int usuarioId);
-    List<Tarefa> findByUsuarioIdOrderByPrioridadeDesc(int usuarioId);
-    List<Tarefa> findByUsuarioIdOrderByStatus(int usuarioId);
-    List<Tarefa> findByUsuarioIdAndStatus(int usuarioId, StatusTarefaEnum statusTarefaEnum);
-    List<Tarefa> findByUsuarioIdAndPrioridade(int usuarioId, PrioridadeTarefaEnum prioridadeTarefaEnum);
-    List<Tarefa> findByStatusAndDataLimiteBefore(StatusTarefaEnum statusTarefaPendente, LocalDateTime agora);
-
-    @Query("SELECT t FROM Tarefa t WHERE t.usuario.id = :usuarioId ORDER BY  t.status ASC , t.prioridade DESC, t.dataLimite ASC")
-    List<Tarefa> findByUsuarioIdOrderByStatusAndPrioridadeAndDataLimite(@Param("usuarioId") int usuarioId);
+    Page<Tarefa> findByUsuarioId(int usuarioId, Pageable pageable);
 }
