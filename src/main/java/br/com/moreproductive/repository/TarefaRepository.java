6f973e2200ef409b110214fa978d588ad68e0f15
Vagner-G-Carneiro <vagnerguimaraescontato@gmail.com>
2025-11-02 00:03:43 -0300

    fix(tarefaRepository): Corrigido o bug por falta da pesquisa expecifica que TarefaScheduling precisa

--- src/main/java/br/com/moreproductive/repository/TarefaRepository.java
+++ src/main/java/br/com/moreproductive/repository/TarefaRepository.java
@@ -1,13 +1,10 @@
 package br.com.moreproductive.repository;
 
 import br.com.moreproductive.entities.Tarefa;
-import br.com.moreproductive.enums.PrioridadeTarefaEnum;
 import br.com.moreproductive.enums.StatusTarefaEnum;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -17,4 +14,5 @@ import java.util.List;
 public interface TarefaRepository extends JpaRepository <Tarefa, Integer> {
 
     Page<Tarefa> findByUsuarioId(int usuarioId, Pageable pageable);
+    List<Tarefa> findByStatusAndDataLimiteBefore(StatusTarefaEnum statusTarefaEnum, LocalDateTime now);
 }
